f8b285b36d7ae9006941ceafcd25ed4a7126feab
Moishe Fischbein <moishefischbein@hotmail.com>
2017-04-04 21:51:07 +0300

    create a new class Geometry

---
+++ src/geometries/Geometry.java
@@ -0,0 +1,28 @@
+package geometries;
+
+/**
+ * Created by Moishe on 04/04/2017.
+ */
+public class Geometry {
+
+
+    private Material _material = new Material();
+    private double _nShininess = 1;
+    private Color _emmission = new Color(0, 0, 0);
+    public abstract List<Point3D> FindIntersections (Ray ray)
+    {
+
+    }
+    public abstract Vector getNormal(Point3D point);
+    public double getShininess();
+    public Material getMaterial();
+    public Color getEmmission();
+    public void setShininess(double n);
+    public void setMaterial(Material _material);
+    public void setEmmission(Color emmission);
+    public void setKs(double ks);
+    public void setKd(double kd);
+    public void setKr(double Kr);
+    public void setKt(double Kt);
+}
+
