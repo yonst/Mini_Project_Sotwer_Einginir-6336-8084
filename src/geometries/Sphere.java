c56f562c7497a31347dcfec0615ac2d9946f32e4
Moishe Fischbein <moishefischbein@hotmail.com>
2017-05-02 18:45:53 +0300

    m

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -4,22 +4,35 @@ package geometries;
  * Created by Moishe on 04/04/2017.
  */
 
-//
+import primitives.Ray;
+import primitives.Point3D;
+import primitives.Vector;
+import primitives.Point2D;
+import primitives.Coordinate;
+import primitives.Material;
+
+import java.util.*;
 
 public class Sphere {
-    /**
-    /**
+
+    private double _radius;
     private Point3D _center;
     // ***************** Constructors ********************** //
-    public Sphere();
-    public Sphere (Sphere sphere);
-    public Sphere(double radius, Point3D center);
-    public Sphere(Map<String, String> attributes);
+    public Sphere(){}
+    public Sphere (Sphere sphere){this(sphere._radius,sphere._center);}
+    public Sphere(double radius, Point3D center){this._radius=radius;this._center=center;}
+    public Sphere(Map<String, String> attributes){}
     // ***************** Getters/Setters ********************** //
-    public Point3D getCenter();
-    public void setCenter(Point3D center);
+    public Point3D getCenter(){return _center;}
+    public void setCenter(Point3D center){_center=center;}
     // ***************** Operations ******************** //
-    public List<Point3D> FindIntersections(Ray ray);
-    public Vector getNormal(Point3D point);
-     */
+    public List<Point3D> FindIntersections(Ray ray){}
+    public Vector getNormal(Point3D point){
+        Vector normal=new Vector();
+        Point3D help3D=this._center;
+        help3D.Substract(point);
+        normal.setHead(help3D);
+        return normal;
+    }
+
 }
