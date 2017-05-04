bf0290a5008acbf1b5a8a0b1bafa9e1bcf6091a6
yonst <yoni1stern@gmail.com>
2017-05-04 17:50:00 +0300

    camera

--- src/Elements/Camera.java
+++ src/Elements/Camera.java
@@ -25,6 +25,13 @@ public class Camera{
     public Camera (Camera camera){
         this(camera._P0,camera._vUp,camera._vTo);
     }
+
+    /**
+     *
+     * @param PO
+     * @param vUp
+     * @param vTo
+     */
     public Camera (Point3D PO, Vector vUp, Vector vTo){
         this._P0= PO;
         this._vUp= vUp;
@@ -45,7 +52,14 @@ public class Camera{
             System.out.print(e);
         }
 
-        this._vRight = vUp.crossProduct(vTo);
+        this._vRight = vTo.crossProduct(vUp);
+        try {
+            _vRight.normalize();
+        }
+        catch (Exception e)
+        {
+            System.out.print(e);
+        }
     }
     public Camera ( Map<String, String> attributes){}
 
@@ -60,12 +74,26 @@ public class Camera{
     public void setP0(Point3D P0){this._P0=P0;}
     public Vector get_vRight(){return _vRight;}
     // ***************** Administration ********************** //
+    @Override
     public String toString()
         {
+
             return "Vto: " + _vTo + "\n" + "Vup: " + _vUp + "\n" + "Vright:" + _vRight + ".";
         }
 
     // ***************** Operations ******************** //
+
+    /**
+     *
+     * @param Nx
+     * @param Ny
+     * @param x
+     * @param y
+     * @param screenDist
+     * @param screenWidth
+     * @param screenHeight
+     * @return
+     */
     public Ray constructRayThroughPixel (int Nx, int Ny,
                                          double x, double y,
                                          double screenDist,
@@ -73,7 +101,7 @@ public class Camera{
                                          double screenHeight)
     {
     Point3D Pc = new Point3D(_P0);
-    Point3D p = new Point3D();
+    Point3D p;
     double Rx = screenWidth/Nx;
     double Ry = screenHeight/Ny;
     Vector tempVtO = new Vector(_vTo);
@@ -81,15 +109,13 @@ public class Camera{
     Vector tempVright = new Vector(_vRight);
     tempVtO.scale(screenDist);
     Pc.add(tempVtO);
-    tempVright.scale(((x-Nx/2)*Rx)+Rx/2);
-    tempVup.scale(((y-Ny/2)*Ry)+Ry/2);
+    tempVright.scale(((x-(double) Nx/2.0)*Rx)+Rx/2.0);
+    tempVup.scale(((y-(double)Ny/2.0)*Ry)+Ry/2.0);
     tempVright.subtract(tempVup);
     Pc.add(tempVright);
     p = new Point3D(Pc);
-    p.Substract(_P0);
-    Vector Ray_vec = new Vector(p);
-    Ray_vec.normalize();
-    Ray R = new Ray(_P0, Ray_vec);
+    Vector Ray_vec = new Vector(p, _P0);
+    Ray R = new Ray(p, Ray_vec);
     return R;
     }
 
