b3d6d16657013e35b52be292f8125d4b827cb41d
Moishe Fischbein <moishefischbein@hotmail.com>
2017-05-12 11:39:33 +0300

    m

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -5,10 +5,11 @@ package geometries;
  */
 import primitives.Point3D;
 import primitives.Vector;
-
+import primitives.Ray;
 import java.util.*;
 
-public class Triangle /*extends Geometry implements FlatGeometry*/ {
+public class Triangle //extends Geometry implements FlatGeometry
+{
 
     private Point3D _p1;
     private Point3D _p2;
@@ -57,7 +58,9 @@ public class Triangle /*extends Geometry implements FlatGeometry*/ {
     }
 
     // ***************** Operations ******************** //
-    public Vector getNormal(Point3D point) {
+
+
+    public Vector getNormal() {
         Vector vec1 = new Vector();
         Vector vec2 = new Vector();
         Vector normalVec = new Vector();
@@ -72,10 +75,61 @@ public class Triangle /*extends Geometry implements FlatGeometry*/ {
         return normalVec;
     }
 
-    /*public List<Point3D> FindIntersections(Ray ray) {
+    public List<Point3D> FindIntersections(Ray ray) {
+    //
+    //.....מצא את P0(נקודת המקור של הקרן)
+    Point3D PO=new Point3D(ray.get_POO());
+    //
+    //...מצא את N( הווקטור הנורמל של המשולש)
+    Vector N= this.getNormal();
+    //
+    //............ .מצא את plane( המישור הנוצר ע"י N ו p3 _של המשולש)
+    Plane plane=new Plane(N,PO);
+    //
+    //.........מצא את נקודות החיתוך בין הקרן למישור. אם אין נקודות חיתוך - החזר רשימה ריקה
+    ArrayList<Point3D> intersection=new ArrayList<>(plane.FindIntersections(ray));
+    Point3D point_inter=new Point3D(intersection.get(0));
+        if (intersection.isEmpty()) {
+            return intersection;
+        }
+    //
+    //................הגדר ווקטור P0_P בין P0 לבין נקודת החיתוך במישור
+    point_inter.subtract(PO);
+    Vector P_PO=new Vector(point_inter);
+    Vector copy_P_PO=new Vector(P_PO);
+        //.............................the 3 vector in the triangle.................................................
+        Vector vec1 = new Vector(new Point3D(ray.get_POO()),new Point3D(_p1));
+        Vector vec2 = new Vector(new Point3D(ray.get_POO()),new Point3D(_p2));
+        Vector vec3 = new Vector(new Point3D(ray.get_POO()),new Point3D(_p3));
+        //.............cross product between the V1 and V2 for the 3 sides of the triangle .........................
+        Vector normal1=new Vector(vec1).crossProduct(new Vector(vec2));
+        Vector normal2=new Vector(vec1).crossProduct(new Vector(vec3));
+        Vector normal3=new Vector(vec2).crossProduct(new Vector(vec3));
+        //........................to normalize the normal1, normal2 and normal3.....................................
+        normal1.normalize();
+        normal2.normalize();
+        normal3.normalize();
+        //חשב את s( .מכפלה סקלרית בין P0_P לבין N בסימן מינוס()
+        double dotProduct1 = copy_P_PO.dotProduct(normal1);
+        double dotProduct2 = copy_P_PO.dotProduct(normal2);
+        double dotProduct3 = copy_P_PO.dotProduct(normal3);
+        /*אם כל 3 ה s הם בעלי אותו סימן )כולם חיוביים או כולם שליליים( זה אומר שהקרן פוגעת במשולש. -< הכנס
+        את נקודת החיתוך במישור לרשימה.*/
+        if ((dotProduct1 > 0 && dotProduct2 > 0 && dotProduct3 > 0) || (dotProduct1 < 0 && dotProduct2 < 0 && dotProduct3 < 0)) {
+            return intersection;
+        } else {
+            intersection.clear();
+            return intersection;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "p1:"+_p1.toString()+" "+"p2:"+_p2.toString()+" "+"p3:"+_p3.toString();
+    }
 
 
-    }*/
+
 }
 
 
