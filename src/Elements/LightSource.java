5b3b08e1524dcaab186527202c7d112607dd42db
Moishe Fischbein <moishefischbein@hotmail.com>
2017-05-15 21:56:23 +0300

    m

--- src/Elements/LightSource.java
+++ src/Elements/LightSource.java
@@ -3,14 +3,20 @@ package Elements;
 /**
  * Created by Moishe on 04/04/2017.
  */
+import java.awt.Color;
+import java.util.Map;
+import primitives.Point2D;
+import primitives.Point3D;
+import primitives.Vector;
+import primitives.Coordinate;
+import primitives.Material;
+
+public abstract class LightSource {
 
 
-public class LightSource {
-    /**
-
-    public abstract Color getIntensity(Point3D point(;
+    public abstract Color getIntensity(Point3D point);
             public abstract Vector getL(Point3D point); // light to point vector
- */
+
 }
 
 
