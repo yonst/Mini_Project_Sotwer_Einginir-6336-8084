0a6b2b060ba1572c3d9786e6da3b2db201d36773
yonst <yoni1stern@gmail.com>
2017-05-15 21:58:15 +0300

    light

--- src/Elements/Light.java
+++ src/Elements/Light.java
@@ -1,16 +1,20 @@
 package Elements;
 
+import java.awt.*;
+
 /**
  * Created by Moishe on 04/04/2017.
  */
 
 public class Light {
-/**
     protected Color _color;
     // ***************** Constructors ********************** //
-    public Light();
-    public Light (Color color);
+    public Light(){}
+    public Light (Color color){
+        _color = new Color(color.getRGB());
+    }
     // ***************** Getters/Setters ********************** //
-    public Color getIntensity();
- */
+    public Color getIntensity(){
+        return new Color(_color.getRGB());
+    }
 }
