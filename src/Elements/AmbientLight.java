af00997f43a22a3be54646d13c95905e637af1e2
Moishe Fischbein <moishefischbein@hotmail.com>
2017-05-15 22:10:25 +0300

    m

--- src/Elements/AmbientLight.java
+++ src/Elements/AmbientLight.java
@@ -3,21 +3,28 @@ package Elements;
 /**
  * Created by Moishe on 04/04/2017.
  */
+import java.awt.Color;
+import java.util.Map;
 
+public class AmbientLight extends Light{
 
-public class AmbientLight {
-/**
 
     private final double _Ka = 0.1;
     // ***************** Constructors ********************** //
-    public AmbientLight();
-    public AmbientLight(AmbientLight aLight);
-    public AmbientLight(int r, int g, int b);
+    public AmbientLight(){}
+    public AmbientLight(AmbientLight aLight)
+    {
+        _color = new Color(aLight._color.getRGB());
+    }
+    public AmbientLight(int r, int g, int b)
+    {
+
+    }
     public AmbientLight(Map<String, String> attributes);
     // ***************** Getters/Setters ********************** //
     public Color getColor();
     public void setColor(Color color);
     public double getKa();
     public Color getIntensity();
- */
+
 }
