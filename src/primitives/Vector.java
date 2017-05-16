7c5d5cf5558db04c6a355cdca47b40937a05ea89
yonst <yoni1stern@gmail.com>
2017-05-16 21:17:41 +0300

    light

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -51,7 +51,7 @@ public class Vector implements Comparable<Vector>{
     }
 
     public Point3D getHead() {
-        return Head;
+        return new Point3D(Head);
     }
 
     // ***************** Administration ******************** //
