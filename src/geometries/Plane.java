dfd788e1ee829c24923dccb3d21eb8faa0f05a42
Moishe Fischbein <moishefischbein@hotmail.com>
2017-05-17 12:01:49 +0300

    m

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -12,44 +12,74 @@ import primitives.Material;
 
 import java.util.*;
 
-public class Plane extends Geometry implements FlatGeometry{
-
+public class Plane extends Geometry implements FlatGeometry {
     private Vector _normal;
     private Point3D _Q;
+
     // ***************** Constructors ********************** //
-    public Plane(){}
-    public Plane (Plane plane)
-    {
-        this(plane._normal,plane._Q);
+    public Plane() {
     }
-    public Plane (Vector normal, Point3D q)
-    {
+
+    public Plane(Plane plane) {
+        this(plane._normal, plane._Q);
+    }
+
+    public Plane(Vector normal, Point3D q) {
         this._normal = normal;
         this._Q = q;
     }
+
     // ***************** Getters/Setters ********************** //
-    public Vector getNormal(Point3D point){return  _normal;}
-    public Point3D getQ(){return _Q;}
-    public void setNormal(Vector normal){this._normal=normal;}
-    public void setQ(Point3D d){this._Q=d;}
-    // ***************** Operations ******************** //
+    public Vector getNormal(Point3D point) {
+        return new Vector(_normal);
+    }
+
+    public Point3D getQ() {
+        return new Point3D(_Q);
+    }
+
+    public void setNormal(Vector normal) {
+        _normal = new Vector(normal);
+    }
+
+    public void setQ(Point3D d) {
+        _Q = new Point3D(d);
+    }
 
 
-    public List<Point3D> FindIntersections(Ray ray) {
-        ArrayList list = new ArrayList();
-        Vector v = new Vector(ray.get_POO());
-        Vector direction = new Vector(ray.get_direction());
-        v.subtract(new Vector(_Q));
-        double f = _normal.dotProduct(direction);
-        if (f != 0) {
-            double t = -(_normal.dotProduct(v) / f);
-            if (t > 0) {
-                direction.scale(t);
-                Point3D p = new Point3D(ray.get_POO());
-                p.add(direction);
-                list.add(p);
-            }
-        }
-        return list;
+
+        // ***************** Operations ******************** //
+  public List<Point3D> FindIntersections(Ray ray) {
+        Point3D PO = new Point3D(ray.get_POO());
+
+        Vector QO = new Vector(_Q);
+        Vector N = new Vector(_normal);
+        Vector V = new Vector(ray.get_direction());
+        Vector PO_QO = new Vector(PO);
+        PO_QO.subtract(QO);
+
+
+        ArrayList intersectionPoint = new ArrayList();
+        double a = N.dotProduct(PO_QO);
+        double b = _normal.dotProduct(ray.get_direction());
+        if (b!=0)
+        {
+        double t = -(a / b);
+
+
+        if (t > 0) {
+            V.scale(t);
+            Point3D po = new Point3D(PO);
+            po.add(V);
+
+            intersectionPoint.add(po);
+        }}
+        return intersectionPoint;
+
+
+
+
+
     }
 }
+
