ba3bb758cf0fc306cd15a5826dd214f1edea0df6
Moishe Fischbein <moishefischbein@hotmail.com>
2017-06-05 20:23:36 +0300

    m

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -3,17 +3,36 @@ package elements;
 /**
  * Created by Moishe on 04/04/2017.
  */
-
+import primitives.Vector;
+import primitives.Point3D;
+import java.awt.Color;
 
 public class DirectionalLight extends Light implements LightSource {
-/**
+
     private Vector _direction;
     // ***************** Constructors ********************** //
-    public DirectionalLight(Color color, Vector direction);
+    public DirectionalLight(Color color, Vector direction)
+    {
+            _color = new Color(color.getRGB());
+            _direction = new Vector(direction);
+
+    }
     // ***************** Getters/Setters ********************** //
-    public Color getIntensity(Point3D point);
-    public Vector getDirection();
-    public void setDirection(Vector _direction);
-    public Vector getL(Point3D point);
- */
+    public Color getIntensity(Point3D point)
+    {
+        return this.getIntensity();
+    }
+    public Vector getDirection()
+    {
+        return new Vector(_direction);
+    }
+    public void setDirection(Vector _direction)
+    {
+        _direction = new Vector(_direction);
+    }
+    public Vector getL(Point3D point)
+    {
+      return new Vector(_direction);
+    }
+
 }
