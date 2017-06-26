ace92f8287edb573f591c0011d774ec0a1329e39
Moishe Fischbein <moishefischbein@hotmail.com>
2017-06-26 14:05:26 +0300

    m

--- src/elements/Light.java
+++ src/elements/Light.java
@@ -5,11 +5,26 @@ import java.awt.*;
 /**
  * Created by Moishe on 04/04/2017.
  */
-
+//an abstract class, defines the color and intensity of each class in the package
 public abstract class Light {
+    //color of the light in each class which extends the class
     protected Color _color;
     // ***************** Constructors ********************** //
+    /*************************************************
+     * Default Constructor
+     * PARAMETERS
+     * none
+     * MEANING
+     * this is a default constructor which defines the color as white
+     **************************************************/
     public Light(){}
+    /*************************************************
+     * Constructor
+     * PARAMETERS
+     * color
+     * MEANING
+     * Initializing the instense of color with green, blue, red.
+     **************************************************/
     public Light (Color color){
         _color = new Color(color.getRed(), color.getGreen(),color.getBlue());
     }
